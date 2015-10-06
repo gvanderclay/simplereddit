69820e9fc5baa28053da02e6b7da16b10316e3ba
Gage Vander Clay <gvanderclay@yahoo.com>
2015-10-06 17:24:55 -0400

    changed the name of the test.fxml to SimpleRedditView.fxml

--- src/com/simplereddit/view/Main.java
+++ src/com/simplereddit/view/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			Parent root = FXMLLoader
-					.load(getClass().getResource("/com/simplereddit/view/test.fxml"));
+					.load(getClass().getResource("/com/simplereddit/view/SimpleRedditView.fxml"));
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(
 					getClass().getResource("application.css").toExternalForm());
