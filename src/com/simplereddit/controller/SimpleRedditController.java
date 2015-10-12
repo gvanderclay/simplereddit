8a21693a3f9a938d26058cf58dcd7adcd8bdd72b
Gage Vander Clay <gvanderclay@yahoo.com>
2015-10-12 13:58:49 -0400

    ADDED MORE FUNCTIONALTY

--- src/com/simplereddit/controller/SimpleRedditController.java
+++ src/com/simplereddit/controller/SimpleRedditController.java
@@ -6,8 +6,11 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.CacheHint;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
@@ -30,7 +33,25 @@ public class SimpleRedditController {
 	private Button newBtn;
 
 	@FXML
-	private Button topBtn;
+	private MenuButton topBtn;
+
+	@FXML
+	private MenuItem topHour;
+
+	@FXML
+	private MenuItem topDay;
+
+	@FXML
+	private MenuItem topWeek;
+
+	@FXML
+	private MenuItem topMonth;
+
+	@FXML
+	private MenuItem topYear;
+
+	@FXML
+	private MenuItem topAll;
 
 	@FXML
 	private TextField subredditTxtBox;
@@ -49,11 +70,11 @@ public class SimpleRedditController {
 
 	@FXML
 	void initialize() {
-
-		// browser.goToLink();;
+		webView.setCache(true);
+		webView.setCacheHint(CacheHint.SPEED);
+		model = new SimpleRedditModel();
 		initWebView();
-//		model = new SimpleRedditModel();
-		webEngine.load(getClass().getResource("ayylmao.html").toExternalForm());
+		webEngine.load(model.getCurrentLink().getUrl());
 
 	}
 
@@ -71,6 +92,76 @@ public class SimpleRedditController {
 		updateWebEngine();
 	}
 
+	@FXML
+	void getFrontPage(ActionEvent event) {
+		model.retrieveFrontPage();
+		updateWebEngine();
+	}
+
+	@FXML
+	void getHotLinks(ActionEvent event) {
+		model.getHotLinks();
+		updateWebEngine();
+	}
+
+	@FXML
+	void getNewLinks(ActionEvent event) {
+		model.getNewLinks();
+		updateWebEngine();
+	}
+
+	@FXML
+	void getTopAll(ActionEvent event) {
+		model.getTopAllLinks();
+		updateWebEngine();
+	}
+
+	@FXML
+	void getTopDay(ActionEvent event) {
+		model.getTopDayLinks();
+		updateWebEngine();
+	}
+
+	@FXML
+	void getTopHour(ActionEvent event) {
+		model.getTopHourLinks();
+		updateWebEngine();
+	}
+
+	@FXML
+	void getTopMonth(ActionEvent event) {
+		model.getTopMonthLinks();
+		updateWebEngine();
+	}
+
+	@FXML
+	void getTopWeek(ActionEvent event) {
+		model.getTopWeekLinks();
+		updateWebEngine();
+	}
+
+	@FXML
+	void getTopYear(ActionEvent event) {
+		model.getTopYearLinks();
+		updateWebEngine();
+	}
+
+	@FXML
+	void goToSubreddit(ActionEvent event) {
+		retrieveSubreddit();
+	}
+
+	@FXML
+	void enterToSubreddit(ActionEvent event) {
+		retrieveSubreddit();
+	}
+
+	private void retrieveSubreddit() {
+		String subreddit = subredditTxtBox.getText();
+		model.retrieveSubreddit(subreddit);
+		updateWebEngine();
+	}
+
 	private void initWebView() {
 		webEngine = webView.getEngine();
 		webEngine.setJavaScriptEnabled(true);
@@ -80,7 +171,7 @@ public class SimpleRedditController {
 				titleLabel.setText(model.getCurrentLink().getTitle());
 			}
 		});
-//		titleLabel.setText(model.getCurrentLink().getTitle());
+		titleLabel.setText(model.getCurrentLink().getTitle());
 	}
 
 	private void updateWebEngine() {
--- src/com/simplereddit/model/SimpleRedditModel.java
+++ src/com/simplereddit/model/SimpleRedditModel.java
@@ -1,8 +1,9 @@
 package com.simplereddit.model;
 
+import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import org.json.JSONArray;
@@ -26,7 +27,11 @@ public class SimpleRedditModel {
 	/**
 	 * Amount of seconds needed to wait between each request
 	 */
-	private final int SECONDS_TO_WAIT_PER_REQUEST = 2;
+	// private final int SECONDS_TO_WAIT_PER_REQUEST = 2;
+
+	private String currentSubreddit;
+
+	private String currentPath;
 
 	/**
 	 * ArrayList of links that the application will store. Will be cleared
@@ -44,13 +49,30 @@ public class SimpleRedditModel {
 	 */
 	private Link currentLink;
 
+	/**
+	 * Index of the currentLink in the links array
+	 */
 	private int currentLinkIndex;
+
+	private boolean topHour;
+
+	private boolean topDay;
+
+	private boolean topWeek;
+
+	private boolean topMonth;
+
+	private boolean topYear;
+
+	private boolean topAll;
 
 	public SimpleRedditModel() {
 		this.links = new ArrayList<Link>();
 		this.linkCount = 0;
+		setAllTopSortsToFalse();
 		writeToHeadersFile("User-Agent:desktop:com.simplereddit:v0.1");
 		retrieveFrontPage();
+		currentSubreddit = "";
 		currentLinkIndex = 0;
 		currentLink = links.get(currentLinkIndex);
 	}
@@ -61,14 +83,199 @@ public class SimpleRedditModel {
 	 */
 	public void retrieveFrontPage() {
 		// clear the links in the links array
-		this.links.clear();
-		this.linkCount = 0;
-		String frontPageJSONString = http.getHttp(
-				SimpleRedditConstants.REDDIT_URL,
-				SimpleRedditConstants.JSON_PATH, null,
-				SimpleRedditConstants.HEADERS_FILE);
+		resetLinks();
+		setAllTopSortsToFalse();
+		currentSubreddit = "";
+		currentPath = "";
+		String frontPageJSONString = retrievePageWithoutParams();
 		JSONObject page = new JSONObject(frontPageJSONString);
 		addPageToLinkArray(page);
+		currentLinkIndex = 0;
+		currentLink = links.get(currentLinkIndex);
+		System.out.println("getting front page");
+	}
+
+	public void retrieveSubreddit(String subreddit) {
+		resetLinks();
+		setAllTopSortsToFalse();
+		currentSubreddit = subreddit;
+		currentPath = "/r/" + subreddit;
+		String subredditJSONString = retrievePageWithoutParams();
+		JSONObject page = new JSONObject(subredditJSONString);
+		addPageToLinkArray(page);
+		currentLinkIndex = 0;
+		currentLink = links.get(currentLinkIndex);
+		System.out.println("Getting subreddit: " + subreddit);
+	}
+
+	public void getHotLinks() {
+		resetLinks();
+		setAllTopSortsToFalse();
+		if (currentSubreddit == "") {
+			currentPath = "";
+		} else {
+			currentPath = "/r/" + currentSubreddit;
+		}
+		String hotJSONString = retrievePageWithoutParams();
+		JSONObject page = new JSONObject(hotJSONString);
+		addPageToLinkArray(page);
+		currentLinkIndex = 0;
+		currentLink = links.get(currentLinkIndex);
+	}
+
+	public void getNewLinks() {
+		resetLinks();
+		setAllTopSortsToFalse();
+		if (currentSubreddit == "") {
+			currentPath = "/new";
+		} else {
+			currentPath = "/r/" + currentSubreddit + "/new";
+		}
+		String newJSONString = retrievePageWithoutParams();
+		JSONObject page = new JSONObject(newJSONString);
+		addPageToLinkArray(page);
+		currentLinkIndex = 0;
+		currentLink = links.get(currentLinkIndex);
+	}
+
+	public void getTopHourLinks() {
+		resetLinks();
+		setAllTopSortsToFalse();
+		this.topHour = true;
+		if (currentSubreddit == "") {
+			currentPath = "/top";
+		} else {
+			currentPath = "/r/" + currentSubreddit + "/top";
+		}
+		writeStringsToFile(false, SimpleRedditConstants.PARAMS_FILE, new String[0]);
+		writeTopSortToParams();
+		String topJSONString = retrievePageWithParams();
+		JSONObject page = new JSONObject(topJSONString);
+		addPageToLinkArray(page);
+		currentLinkIndex = 0;
+		currentLink = links.get(currentLinkIndex);
+	}
+
+	public void getTopDayLinks() {
+		resetLinks();
+		setAllTopSortsToFalse();
+		this.topDay = true;
+		if (currentSubreddit == "") {
+			currentPath = "/top";
+		} else {
+			currentPath = "/r/" + currentSubreddit + "/top";
+		}
+		writeStringsToFile(false, SimpleRedditConstants.PARAMS_FILE, new String[0]);
+		writeTopSortToParams();
+		String topJSONString = retrievePageWithParams();
+		JSONObject page = new JSONObject(topJSONString);
+		addPageToLinkArray(page);
+		currentLinkIndex = 0;
+		currentLink = links.get(currentLinkIndex);
+	}
+
+	public void getTopWeekLinks() {
+		resetLinks();
+		setAllTopSortsToFalse();
+		this.topWeek = true;
+		if (currentSubreddit == "") {
+			currentPath = "/top";
+		} else {
+			currentPath = "/r/" + currentSubreddit + "/top";
+		}
+		writeStringsToFile(false, SimpleRedditConstants.PARAMS_FILE, new String[0]);
+		writeTopSortToParams();
+		String topJSONString = retrievePageWithParams();
+		JSONObject page = new JSONObject(topJSONString);
+		addPageToLinkArray(page);
+		currentLinkIndex = 0;
+		currentLink = links.get(currentLinkIndex);
+	}
+
+	public void getTopMonthLinks() {
+		resetLinks();
+		setAllTopSortsToFalse();
+		this.topMonth = true;
+		if (currentSubreddit == "") {
+			currentPath = "/top";
+		} else {
+			currentPath = "/r/" + currentSubreddit + "/top";
+		}
+		writeStringsToFile(false, SimpleRedditConstants.PARAMS_FILE, new String[0]);
+		writeTopSortToParams();
+		String topJSONString = retrievePageWithParams();
+		JSONObject page = new JSONObject(topJSONString);
+		addPageToLinkArray(page);
+		currentLinkIndex = 0;
+		currentLink = links.get(currentLinkIndex);
+	}
+
+	public void getTopYearLinks() {
+		resetLinks();
+		setAllTopSortsToFalse();
+		this.topYear = true;
+		if (currentSubreddit == "") {
+			currentPath = "/top";
+		} else {
+			currentPath = "/r/" + currentSubreddit + "/top";
+		}
+		writeStringsToFile(false, SimpleRedditConstants.PARAMS_FILE, new String[0]);
+		writeTopSortToParams();
+		String topJSONString = retrievePageWithParams();
+		JSONObject page = new JSONObject(topJSONString);
+		addPageToLinkArray(page);
+		currentLinkIndex = 0;
+		currentLink = links.get(currentLinkIndex);
+	}
+
+	public void getTopAllLinks() {
+		resetLinks();
+		setAllTopSortsToFalse();
+		this.topAll = true;
+		if (currentSubreddit == "") {
+			currentPath = "/top";
+		} else {
+			currentPath = "/r/" + currentSubreddit + "/top";
+		}
+		writeStringsToFile(false, SimpleRedditConstants.PARAMS_FILE, new String[0]);
+		writeTopSortToParams();
+		String topJSONString = retrievePageWithParams();
+		JSONObject page = new JSONObject(topJSONString);
+		addPageToLinkArray(page);
+		currentLinkIndex = 0;
+		currentLink = links.get(currentLinkIndex);
+	}
+
+	private void resetLinks() {
+		this.links.clear();
+		this.linkCount = 0;
+
+	}
+
+	private void writeTopSortToParams() {
+		if (topHour) {
+			writeStringsToFile(true, SimpleRedditConstants.PARAMS_FILE, "sort:top", "t:hour");
+		} else if (topDay) {
+			writeStringsToFile(true, SimpleRedditConstants.PARAMS_FILE, "sort:top", "t:day");
+		} else if (topWeek) {
+			writeStringsToFile(true, SimpleRedditConstants.PARAMS_FILE, "sort:top", "t:week");
+		} else if (topMonth) {
+			writeStringsToFile(true, SimpleRedditConstants.PARAMS_FILE, "sort:top", "t:month");
+		} else if (topYear) {
+			writeStringsToFile(true, SimpleRedditConstants.PARAMS_FILE, "sort:top", "t:year");
+		} else if (topAll) {
+			writeStringsToFile(true, SimpleRedditConstants.PARAMS_FILE, "sort:top", "t:all");
+		}
+
+	}
+
+	private void setAllTopSortsToFalse() {
+		this.topHour = false;
+		this.topDay = false;
+		this.topWeek = false;
+		this.topMonth = false;
+		this.topYear = false;
+		this.topAll = false;
 	}
 
 	/**
@@ -95,11 +302,8 @@ public class SimpleRedditModel {
 		String lastLinkId = lastLink.getId();
 		// format the params to query for the next page
 		writeToParamsFile("count:" + linkCount, "after:" + lastLinkId);
-		String nextPageJSONString = http.getHttp(
-				SimpleRedditConstants.REDDIT_URL,
-				SimpleRedditConstants.JSON_PATH,
-				SimpleRedditConstants.PARAMS_FILE,
-				SimpleRedditConstants.HEADERS_FILE);
+		writeTopSortToParams();
+		String nextPageJSONString = retrievePageWithParams();
 		JSONObject page = new JSONObject(nextPageJSONString);
 		addPageToLinkArray(page);
 	}
@@ -114,11 +318,6 @@ public class SimpleRedditModel {
 			return;
 		}
 		Link firstLink = this.links.get(0);
-		// If we are on the first page, we can't get the previous page
-		if (this.linkCount == 0 || this.linkCount == 26) {
-			System.out.println("CAN'T DO THAT");
-			return;
-		}
 		// If the link count is divisible by 25, we need to adjust
 		if (this.linkCount % LINKS_PER_PAGE == 1) {
 			this.linkCount -= LINKS_PER_PAGE;
@@ -129,16 +328,16 @@ public class SimpleRedditModel {
 		String firstLinkId = firstLink.getId();
 		// format the params
 		writeToParamsFile("count:" + this.linkCount, "before:" + firstLinkId);
-		String prevPageJSONString = http.getHttp(
-				SimpleRedditConstants.REDDIT_URL,
-				SimpleRedditConstants.JSON_PATH,
-				SimpleRedditConstants.PARAMS_FILE,
-				SimpleRedditConstants.HEADERS_FILE);
+		writeTopSortToParams();
+		String prevPageJSONString = retrievePageWithParams();
 		JSONObject page = new JSONObject(prevPageJSONString);
 		addPageToLinkArray(page);
 	}
 
 	public Link getNextLink() {
+		if (currentLinkIndex >= links.size() - 1 && links.size() < LINKS_PER_PAGE) {
+			currentLinkIndex = -1;
+		}
 		if (currentLinkIndex >= LINKS_PER_PAGE - 1) {
 			currentLinkIndex = -1;
 			retrieveNextPage();
@@ -148,12 +347,24 @@ public class SimpleRedditModel {
 	}
 
 	public Link getPreviousLink() {
+		System.out.println(atFirstLink());
+		if (atFirstLink()) {
+			System.out.println("CAN'T DO THAT");
+			return this.currentLink;
+		}
 		if (currentLinkIndex == 0) {
 			retrievePreviousPage();
-			currentLinkIndex = 1;
+			currentLinkIndex = 25;
 		}
 		this.currentLink = links.get(--currentLinkIndex);
 		return this.currentLink;
+	}
+
+	public boolean atFirstLink() {
+		if (this.currentLinkIndex == 0 && (this.linkCount == 0 || this.linkCount == 26)) {
+			return true;
+		}
+		return false;
 	}
 
 	public Link getCurrentLink() {
@@ -162,12 +373,11 @@ public class SimpleRedditModel {
 
 	/**
 	 * Adds a page of reddit to the link array
-	 * 
+	 *
 	 * @param page
 	 */
 	private void addPageToLinkArray(JSONObject page) {
-		JSONArray JSONLinks = page.getJSONObject("data").getJSONArray(
-				"children");
+		JSONArray JSONLinks = page.getJSONObject("data").getJSONArray("children");
 		for (int i = 0; i < JSONLinks.length(); i++) {
 			JSONObject JSONLink = JSONLinks.getJSONObject(i);
 			this.links.add(convertJSONLinkToLink(JSONLink));
@@ -176,7 +386,7 @@ public class SimpleRedditModel {
 
 	/**
 	 * Converts a specific link that is in a JSONObject into a Link
-	 * 
+	 *
 	 * @param JSONLink
 	 *            Link from reddit that is in JSON
 	 * @return the converted Link object
@@ -195,7 +405,7 @@ public class SimpleRedditModel {
 
 	/**
 	 * Gets the links on the current page
-	 * 
+	 *
 	 * @return the links on the current page
 	 */
 	public ArrayList<Link> getLinks() {
@@ -204,7 +414,7 @@ public class SimpleRedditModel {
 
 	/**
 	 * Set the links on the current page
-	 * 
+	 *
 	 * @param links
 	 *            links to set as the current page
 	 */
@@ -212,79 +422,89 @@ public class SimpleRedditModel {
 		this.links = links;
 	}
 
+	private String retrievePageWithParams() {
+		return http.getHttp(SimpleRedditConstants.REDDIT_URL, currentPath + SimpleRedditConstants.JSON_PATH,
+				SimpleRedditConstants.PARAMS_FILE, SimpleRedditConstants.HEADERS_FILE);
+	}
+
+	private String retrievePageWithoutParams() {
+		return http.getHttp(SimpleRedditConstants.REDDIT_URL, currentPath + SimpleRedditConstants.JSON_PATH, null,
+				SimpleRedditConstants.HEADERS_FILE);
+	}
+
 	/**
-	 * Add parameters to the parameters file
-	 * 
+	 * Clear the parameters file and add parameters to the parameters file
+	 *
 	 * @param params
 	 *            The parameters that will be added in the form
 	 *            "paramtitle":"paraminfo" without the quotes
 	 */
 	private void writeToParamsFile(String... params) {
-		StringBuilder sb = new StringBuilder();
-		for (String param : params) {
-			sb.append(param + "\n");
-			writeStringToFile(sb.toString(), SimpleRedditConstants.PARAMS_FILE);
+		writeStringsToFile(false, SimpleRedditConstants.PARAMS_FILE, params);
+	}
+
+	public void writeStringsToFile(boolean append, File file, String... params) {
+		BufferedWriter bw = null;
+		try {
+			// APPEND MODE SET HERE
+			bw = new BufferedWriter(new FileWriter(file, append));
+			for (String param : params) {
+				bw.write(param);
+				bw.newLine();
+				bw.flush();
+			}
+
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		} finally { // always close the file
+			if (bw != null)
+				try {
+					bw.close();
+				} catch (IOException ioe2) {
+					// just ignore it
+				}
 		}
 	}
 
 	/**
 	 * Add headers for the http request
-	 * 
+	 *
 	 * @param headers
 	 *            The headers that will be added to the headers file in the form
 	 *            "headertitle":"headerinfo" without the quotes
 	 */
 	private void writeToHeadersFile(String... headers) {
-		StringBuilder sb = new StringBuilder();
-		for (String header : headers) {
-			sb.append(header + "\n");
-			writeStringToFile(sb.toString(), SimpleRedditConstants.HEADERS_FILE);
-		}
-	}
-
-	/**
-	 * Writes a string to a file
-	 * 
-	 * @param stringToWrite
-	 *            string to be written to the file
-	 * @param file
-	 *            file being written to
-	 */
-	private void writeStringToFile(String stringToWrite, File file) {
-		try {
-			PrintWriter writer = new PrintWriter(file);
-			writer.print(stringToWrite);
-			writer.close();
-		} catch (FileNotFoundException e) {
-			System.out.println("Params file not found");
-		}
-
+		writeStringsToFile(false, SimpleRedditConstants.HEADERS_FILE, headers);
 	}
 
 	/**
 	 * Wait a certain amount of seconds Used so we don't go over the maximum
 	 * amount of requests per minute
-	 * 
+	 *
 	 * @param seconds
 	 */
-	private void waitSeconds(int seconds) {
-		try {
-			Thread.sleep(1000 * seconds); // 1000 milliseconds is one second.
-		} catch (InterruptedException ex) {
-			Thread.currentThread().interrupt();
-		}
-	}
+	// private void waitSeconds(int seconds) {
+	// try {
+	// Thread.sleep(1000 * seconds); // 1000 milliseconds is one second.
+	// } catch (InterruptedException ex) {
+	// Thread.currentThread().interrupt();
+	// }
+	// }
 
 	public static void main(String args[]) {
-		SimpleRedditModel mod = new SimpleRedditModel();
-		System.out.println(mod.getCurrentLink());
-		for (int i = 0; i < 10; i++) {
-			System.out.println(mod.getNextLink());
-		}
-		
-		for(int i = 0; i < 1000; i++){
-			System.out.println(mod.getPreviousLink());
-		}
+		// SimpleRedditModel mod = new SimpleRedditModel();
+		//
+		// // mod.retrieveSubreddit("hiphopheads");
+		//
+		// System.out.println(mod.getCurrentLink());
+		//
+		// for (int i = 0; i < 26; i++) {
+		// System.out.println(mod.getNextLink());
+		// }
+		// System.out.println("ayy");
+		// for (int i = 0; i < 10; i++) {
+		// System.out.println(mod.getPreviousLink());
+		// }
 
 		// for (int i = 0; i < 10; i++) {
 		// System.out.println("GETTING PAGE NUMBER: " + i);
